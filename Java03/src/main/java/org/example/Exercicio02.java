57dbd7f6146dfe2c555772e61a442dd12a46e273
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-08-13 20:36:15 -0300

    Trabalhando com vetores

---
+++ Java03/src/main/java/org/example/Exercicio02.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Exercicio02 {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        ArrayList<Double> notas = new ArrayList<>();
+        String resposta;
+
+        do{
+            System.out.println("Digite uma nota: ");
+            notas.add(ler.nextDouble());
+
+            System.out.println("Deseja inserir mais uma nota? ");
+            System.out.println("Pressione a tecla N para sair.");
+            resposta = ler.next();
+        } while (!resposta.equalsIgnoreCase("n"));
+
+        System.out.println("\nExibindo as notas: ");
+        for (double nota:notas){
+            System.out.println("Nota: " +nota);
+        }
+    }
+}
