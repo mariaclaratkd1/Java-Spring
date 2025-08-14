c4204d6b3228fda09553af9b1f7631e1dea654d2
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-08-13 21:37:48 -0300

    ajustando

--- Java03/src/main/java/org/example/Exercicio02.java
+++ Java03/src/main/java/org/example/Exercicio02.java
@@ -1,6 +1,4 @@
 package org.example;
-
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -8,15 +6,27 @@ public class Exercicio02 {
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
         ArrayList<Double> notas = new ArrayList<>();
-        String resposta;
+        String resposta = "";
 
         do{
-            System.out.println("Digite uma nota: ");
-            notas.add(ler.nextDouble());
+            double nota1, nota2;
 
-            System.out.println("Deseja inserir mais uma nota? ");
-            System.out.println("Pressione a tecla N para sair.");
-            resposta = ler.next();
+            System.out.println("Digite uma nota: ");
+            nota1 = ler.nextDouble();
+            System.out.println("Digite outra nota: ");
+            nota2 = ler.nextDouble();
+
+            if ((nota1 >= 0 && nota1 <= 10) && (nota2 >= 0 && nota2 <= 10)) {
+                notas.add(nota1);
+                notas.add(nota2);
+            } else {
+                System.out.println("Uma ou ambas as notas são inválidas. Digite novamente valores entre 0 e 10.");
+                continue;
+            }
+
+            System.out.println("Deseja inserir mais uma nota? (Pressione a tecla N para sair.)");
+            ler.nextLine();
+            resposta = ler.nextLine();
         } while (!resposta.equalsIgnoreCase("n"));
 
         System.out.println("\nExibindo as notas: ");
