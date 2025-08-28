7823db6707b6fcf270972a41d39354ca9408bcbb
Maria Clara Abreu do Nascimento <mariatestexbox@gmail.com>
2025-08-27 21:22:01 -0300

    Delete Aula03 directory

--- Aula03/src/main/java/org/example/Main.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-        Funcionario funcionario1 = new Funcionario(1,"Maria", 18, 15000.0, Setor.VENDAS, Sexo.FEMININO);
-
-        System.out.println(funcionario1.toString());
-    }
-}
