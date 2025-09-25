6325f0aee64c7e0076ec8ab553e7ca147445504f
Maria Clara Abreu do Nascimento <mariatestexbox@gmail.com>
2025-09-25 00:29:20 -0300

    finalizando exericico de repetição

--- Exercicio/Exercicio/src/main/java/com/example/Exercicio/controller/UsuarioController.java
+++ Exercicio/Exercicio/src/main/java/com/example/Exercicio/controller/UsuarioController.java
@@ -1,4 +1,30 @@
 package com.example.Exercicio.controller;
 
+import com.example.Exercicio.model.Usuario;
+import com.example.Exercicio.service.UsuarioService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/usuario")
 public class UsuarioController {
+    private UsuarioService usuarioService;
+
+    public UsuarioController (UsuarioService usuarioService){
+        this.usuarioService = usuarioService;
+    }
+
+    @GetMapping
+    public List<Usuario> listarUsuarios(){
+        return usuarioService.listarTodos();
+    }
+
+    @PostMapping
+    public ResponseEntity<Usuario> salvarUsuarios (@RequestBody Usuario usuario){
+        usuarioService.salvar(usuario);
+        return ResponseEntity.status(HttpStatus.CREATED).body(usuario);
+    }
 }
